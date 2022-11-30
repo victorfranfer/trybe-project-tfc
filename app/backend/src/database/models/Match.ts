c27451eb2ee0b32e366fcf4ea24b962cf02c5423
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-11-29 21:09:27 -0300

    migration-models-incompleto

---
+++ app/backend/src/database/models/Match.ts
@@ -0,0 +1,59 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Team from './Team';
+
+class Match extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: boolean;
+}
+
+Match.init({
+  id: {
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+    type: INTEGER,
+  },
+  homeTeam: {
+    allowNull: false,
+    type: INTEGER,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  homeTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  awayTeam: {
+    allowNull: false,
+    type: INTEGER,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  awayTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  inProgress: {
+    allowNull: false,
+    type: BOOLEAN,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+  modelName: 'matches',
+});
+
+Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'teamHome' });
+Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+export default Match;
---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,27 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Team.init({
+  id: {
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+    type: INTEGER,
+  },
+  teamName: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+  modelName: 'teams',
+});
+
+export default Team;
